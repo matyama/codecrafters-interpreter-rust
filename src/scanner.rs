0541ae003c1efdc1e2373bb31f2adaa366d1e7a0
Martin Matyášek <martin.matyasek@gmail.com>
2024-07-30 10:01:45 +0200

    refactor: move `Token` to its own module

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,172 +1,10 @@
-use std::collections::HashMap;
 use std::fmt::Display;
 use std::ops::{Range, RangeInclusive};
 use std::process::{ExitCode, Termination};
-use std::sync::OnceLock;
 
+use crate::token::{Literal, Token, TokenType, Tokens};
 use crate::Report;
 use matcher::*;
-
-static KEYWORDS: OnceLock<HashMap<&str, TokenType>> = OnceLock::new();
-
-fn init_keywords() -> HashMap<&'static str, TokenType> {
-    let mut keywords = HashMap::with_capacity(16);
-
-    keywords.insert("and", TokenType::And);
-    keywords.insert("class", TokenType::Class);
-    keywords.insert("else", TokenType::Else);
-    keywords.insert("false", TokenType::False);
-    keywords.insert("for", TokenType::For);
-    keywords.insert("fun", TokenType::Fun);
-    keywords.insert("if", TokenType::If);
-    keywords.insert("nil", TokenType::Nil);
-    keywords.insert("or", TokenType::Or);
-    keywords.insert("print", TokenType::Print);
-    keywords.insert("return", TokenType::Return);
-    keywords.insert("super", TokenType::Super);
-    keywords.insert("this", TokenType::This);
-    keywords.insert("true", TokenType::True);
-    keywords.insert("var", TokenType::Var);
-    keywords.insert("while", TokenType::While);
-
-    keywords
-}
-
-#[allow(clippy::upper_case_acronyms)]
-#[derive(Clone, Copy, Debug)]
-pub enum TokenType {
-    // Single character tokens
-    LeftParen,
-    RightParen,
-    LeftBrace,
-    RightBrace,
-    Comma,
-    Dot,
-    Minus,
-    Plus,
-    Semicolon,
-    Slash,
-    Star,
-
-    // One or two character tokens
-    Bang,
-    BangEqual,
-    Equal,
-    EqualEqual,
-    Greater,
-    GreaterEqual,
-    Less,
-    LessEqual,
-
-    // Literals
-    Identifier,
-    String,
-    Number,
-
-    // Keywords
-    And,
-    Class,
-    Else,
-    False,
-    Fun,
-    For,
-    If,
-    Nil,
-    Or,
-    Print,
-    Return,
-    Super,
-    This,
-    True,
-    Var,
-    While,
-
-    // End Of File
-    EOF,
-}
-
-impl Display for TokenType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::LeftParen => write!(f, "LEFT_PAREN"),
-            Self::RightParen => write!(f, "RIGHT_PAREN"),
-            Self::LeftBrace => write!(f, "LEFT_BRACE"),
-            Self::RightBrace => write!(f, "RIGHT_BRACE"),
-            Self::Comma => write!(f, "COMMA"),
-            Self::Dot => write!(f, "DOT"),
-            Self::Minus => write!(f, "MINUS"),
-            Self::Plus => write!(f, "PLUS"),
-            Self::Semicolon => write!(f, "SEMICOLON"),
-            Self::Slash => write!(f, "SLASH"),
-            Self::Star => write!(f, "STAR"),
-            Self::Bang => write!(f, "BANG"),
-            Self::BangEqual => write!(f, "BANG_EQUAL"),
-            Self::Equal => write!(f, "EQUAL"),
-            Self::EqualEqual => write!(f, "EQUAL_EQUAL"),
-            Self::Greater => write!(f, "GREATER"),
-            Self::GreaterEqual => write!(f, "GREATER_EQUAL"),
-            Self::Less => write!(f, "LESS"),
-            Self::LessEqual => write!(f, "LESS_EQUAL"),
-            Self::Identifier => write!(f, "IDENTIFIER"),
-            Self::String => write!(f, "STRING"),
-            Self::Number => write!(f, "NUMBER"),
-            Self::And => write!(f, "AND"),
-            Self::Class => write!(f, "CLASS"),
-            Self::Else => write!(f, "ELSE"),
-            Self::False => write!(f, "FALSE"),
-            Self::Fun => write!(f, "FUN"),
-            Self::For => write!(f, "FOR"),
-            Self::If => write!(f, "IF"),
-            Self::Nil => write!(f, "NIL"),
-            Self::Or => write!(f, "OR"),
-            Self::Print => write!(f, "PRINT"),
-            Self::Return => write!(f, "RETURN"),
-            Self::Super => write!(f, "SUPER"),
-            Self::This => write!(f, "THIS"),
-            Self::True => write!(f, "TRUE"),
-            Self::Var => write!(f, "VAR"),
-            Self::While => write!(f, "WHILE"),
-            Self::EOF => write!(f, "EOF"),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub enum Literal {
-    Str(String),
-    Num(f64),
-}
-
-impl Display for Literal {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Str(s) => write!(f, "{s}"),
-            Self::Num(n) if n.fract() > 0.0 => write!(f, "{n}"),
-            Self::Num(n) => write!(f, "{n:.1}"),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct Token {
-    ty: TokenType,
-    lexeme: String,
-    literal: Option<Literal>,
-    #[allow(dead_code)]
-    line: usize,
-}
-
-impl Display for Token {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let Some(ref literal) = self.literal {
-            write!(f, "{} {} {}", self.ty, self.lexeme, literal)
-        } else {
-            write!(f, "{} {} null", self.ty, self.lexeme)
-        }
-    }
-}
-
-pub type Tokens = Vec<Token>;
 
 macro_rules! add_token {
     ($s:ident; if $lit:literal { $x:ident } else { $y:ident }) => {{
@@ -365,15 +203,8 @@ impl Scanner {
 
                 c if alpha(c) => {
                     self.advance_until(non_alphanumeric, noop);
-
                     let text = self.text();
-
-                    let ty = KEYWORDS
-                        .get_or_init(init_keywords)
-                        .get(text.as_str())
-                        .copied()
-                        .unwrap_or(TokenType::Identifier);
-
+                    let ty = TokenType::kw_or_ident(text);
                     self.add_token(ty, None);
                 }
 
@@ -384,13 +215,7 @@ impl Scanner {
             }
         }
 
-        // NOTE: specifically not using `add_non_lit` to trim trailing newline for the lexeme
-        self.tokens.push(Token {
-            ty: TokenType::EOF,
-            lexeme: String::new(),
-            literal: None,
-            line: self.line,
-        });
+        self.tokens.push(Token::eof(self.line));
 
         if self.report.had_error {
             Err((self.tokens, LexError))
